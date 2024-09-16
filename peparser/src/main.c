1f6a356238eae8dfaf9879a0940f847f335ed0d2
nova <nv@tower.com>
2024-09-16 14:58:02 +0200

    added NT-, FILE- and OPTIONAL-Header

--- peparser/src/main.c
+++ peparser/src/main.c
@@ -19,13 +19,35 @@ int main() {
 		return 1;
 	}
 	
-	printf("Module base address: 0x%X \n", baseAddr);
+	printf("\tModule base address: 0x00%X \n", baseAddr);
 
 	printf("----DOS HEADER----\n");
 	PIMAGE_DOS_HEADER pDosHdr = (PIMAGE_DOS_HEADER) baseAddr;
 
-	printf("Dos Header Signature: %s\n", (CHAR*)pDosHdr);
-		
+	printf("\tDos Header Signature: %s\n", (CHAR*)pDosHdr);
+
+	printf("----IMAGE NT HEADER----\n");
+
+	PIMAGE_NT_HEADERS pNtHdr = (PIMAGE_NT_HEADERS)(baseAddr + pDosHdr->e_lfanew);
+
+	printf("\tPE Header Signature: %s\n", (CHAR*)pNtHdr);
+
+	printf("----IMAGE FILE HEADER----\n");
+
+	PIMAGE_FILE_HEADER pFileHdr = (PIMAGE_FILE_HEADER)&pNtHdr->FileHeader;
+
+	printf("\tCPU type: 0x%X\n", pFileHdr->Machine);
+	printf("\tNumber of sections: %d\n", pFileHdr->NumberOfSections);
+	printf("\tCharacteristics: 0x%X\n", pFileHdr->Characteristics);
+
+	printf("----IMAGE OPTIONAL HEADER----\n");
+
+	PIMAGE_OPTIONAL_HEADER pOptHdr = (PIMAGE_OPTIONAL_HEADER)&pNtHdr->OptionalHeader;
+
+	printf("\tAddress of entry point: 0x00%X\n", (baseAddr + pOptHdr->AddressOfEntryPoint));
+	printf("\tImage Base: 0x00%X\n", pOptHdr->ImageBase);
+
+
 	return 0;
 
 }
